f7d860725974e138fbb8ab8062cd0ec5c3955126
fzsource <63889910+fzsource@users.noreply.github.com>
2024-02-17 21:18:35 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -9,6 +9,8 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
