bffbb85a896cee6ffbf93bdc333520641a5eb73c
fzsource <63889910+fzsource@users.noreply.github.com>
2024-02-21 12:05:09 +0800

    Update diy-part1.sh

--- config/lede-n1-kenzok8/diy-part1.sh
+++ config/lede-n1-kenzok8/diy-part1.sh
@@ -9,9 +9,9 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git amlogic https://github.com/ophub/luci-app-amlogic' feeds.conf.default
+  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git amlogic https://github.com/ophub/luci-app-amlogic' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
