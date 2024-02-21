fa23e5b467e97b122cb7dde957a052614fd9adf1
fzsource <63889910+fzsource@users.noreply.github.com>
2024-02-21 12:02:59 +0800

    Update diy-part2.sh

--- config/Lienol-master/diy-part2.sh
+++ config/Lienol-master/diy-part2.sh
@@ -24,7 +24,7 @@ echo "DISTRIB_SOURCECODE='Lienol'" >>package/base-files/files/etc/openwrt_releas
 #
 # Add luci-app-amlogic
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-# git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+  git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
